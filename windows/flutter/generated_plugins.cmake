46694490136109767d92f8e666f626c42e826c41
Paul Gregoire <mondain@gmail.com>
2025-12-28 15:13:11 -0800

    Update native build files

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_windows_video
   media_kit_video
   permission_handler_windows
-  screen_brightness_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
